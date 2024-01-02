c891ba0544fe8b4a373e3d0f2e59f16f89073e96
Ahmed <rifkiahmed.91@gmail.com>
2024-01-02 23:30:06 +0000

    Create an employees object

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,12 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  let str = '';
-  for (const employee of employees) {
-    if (str === '') {
-      str += `${employee}`;
-    } else {
-      str += `, ${employee}`;
-    }
-  }
-
-  return `{ ${departmentName}: [ ${str} ] }`;
+  return { [`${departmentName}`]: employees };
 }
