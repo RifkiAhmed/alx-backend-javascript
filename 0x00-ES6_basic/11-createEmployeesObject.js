606b5003beb594dff691b295a3258e38ede1d959
Ahmed <rifkiahmed.91@gmail.com>
2024-01-02 22:58:33 +0000

    Create a string that represnets an object

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,12 @@
+export default function createEmployeesObject(departmentName, employees) {
+  let str = '';
+  for (const employee of employees) {
+    if (str === '') {
+      str += `'${employee}'`;
+    } else {
+      str += `, '${employee}'`;
+    }
+  }
+
+  return `{ ${departmentName}: [ ${str} ] }`;
+}
