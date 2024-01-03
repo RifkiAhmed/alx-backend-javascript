afc422369e21ee77545f313acc621b378fd15e75
Ahmed <rifkiahmed.91@gmail.com>
2024-01-03 22:25:11 +0000

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  return Promise.allSettled([userPromise, photoPromise])
+    .then(([userResult, photoResult]) => [
+      {
+        status: userResult.status,
+        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
+      },
+      {
+        status: photoResult.status,
+        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason,
+      },
+    ])
+    .catch((error) => {
+      console.log(error);
+    });
+}
