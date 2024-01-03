2742bbcab67bbca764d4e5f35fcadd626a07de15
Ahmed <rifkiahmed.91@gmail.com>
2024-01-03 19:06:33 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const res1 = uploadPhoto();
-  const res2 = createUser();
-  Promise.all([res1, res2]).then((values) => {
-    const [photo, user] = values;
-    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  });
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    });
 }
