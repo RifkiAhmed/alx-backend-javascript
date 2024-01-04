49d1b6863e87741303835db3c357bb662304dc4b
Ahmed <rifkiahmed.91@gmail.com>
2024-01-04 19:39:38 +0000

    Implement class Currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else { throw new TypeError('Code must be a string'); }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else { throw new TypeError('Name must be a string'); }
+  }
+
+  get name() {
+    return this._name;
+  }
+}
