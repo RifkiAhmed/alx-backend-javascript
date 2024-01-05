8bca9209038a7cd96c2a08a5dfd609f81155a1c0
Ahmed <rifkiahmed.91@gmail.com>
2024-01-05 00:27:11 +0000

    Implement EVCar class that inherits from Car

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-underscore-dangle */
+
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+
+  /* eslint-disable-next-line class-methods-use-this */
+  cloneCar() {
+    return new Car();
+  }
+}
