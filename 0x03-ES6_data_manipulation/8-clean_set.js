b33f20338b09107cbc3cc25c80762d237083b308
Ahmed <rifkiahmed.91@gmail.com>
2024-01-09 20:52:28 +0000

    Return a string of all the set values that start with a specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,12 @@
 export default function cleanSet(set, startString) {
   let string = '';
-  if (!startString || startString === '') {
-    return string;
+  if (typeof startString === 'string' && startString !== '') {
+    set.forEach((element) => {
+      if (element.startsWith(startString)) {
+        string += (string.length === 0)
+          ? element.slice(startString.length) : `-${element.slice(startString.length)}`;
+      }
+    });
   }
-  set.forEach((element) => {
-    if (element.startsWith(startString)) {
-      string += (string.length === 0)
-        ? element.slice(startString.length) : `-${element.slice(startString.length)}`;
-    }
-  });
   return string;
 }
