0bca7749d70c9a3f95e9b3d91ed77c65279d662f
Ahmed <rifkiahmed.91@gmail.com>
2024-01-09 22:29:00 +0000

    Return an updated map for all items with initial quantity at 1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,7 @@
 export default function updateUniqueItems(map) {
-  const array = Array.from(map.keys());
-  array.map((element) => map.get(element) === 1 && map.set(element, 100));
-
-  return map;
+  if (map instanceof Map) {
+    map.forEach((value, key) => value === 1 && map.set(key, 100));
+    return map;
+  }
+  throw new Error('Cannot process');
 }
