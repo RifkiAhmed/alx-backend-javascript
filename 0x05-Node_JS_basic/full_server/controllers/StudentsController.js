c1669e214059c0b6f0c65c5098b079418cf99603
ahmed <rifkiahmed.91@gmail.com>
2024-02-27 23:51:21 +0100

    Fix semistandard check

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,37 +1,37 @@
 import readDatabase from '../utils';
 
 class StudentsController {
-  static getAllStudents(req, res){
+  static getAllStudents(request, response){
     readDatabase(process.argv[2])
     .then((data) => {
-      res.status(200);
+      response.status(200);
       let message = "";
       for (let field in data) {
         message += `\nNumber of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}`;
       }
-      res.send(`This is the list of our students${message}`);
+      response.send(`This is the list of our students${message}`);
     })
     .catch((error) => {
-      res.status(500);
-      res.send(error.message);
+      response.status(500);
+      response.send(error.message);
     });
   }
 
-  static getAllStudentsByMajor(req, res) {
-    const { major } = req.params;
+  static getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
     if (major && (major === 'CS' || major === 'SWE')) {
       readDatabase(process.argv[2])
       .then((data) => {
-        res.status(200);
-        res.send(`List: ${data[major].join(', ')}`);
+        response.status(200);
+        response.send(`List: ${data[major].join(', ')}`);
       })
       .catch((error) => {
-        res.status(500);
-        res.send(error.message);
+        response.status(500);
+        response.send(error.message);
       });
     } else {
-      res.status(500);
-      res.send('Major parameter must be CS or SWE');
+      response.status(500);
+      response.send('Major parameter must be CS or SWE');
     }
   }
 }
