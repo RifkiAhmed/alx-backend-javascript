191c7261ad893da311c167f22ba873d5ceb73591
ahmed <rifkiahmed.91@gmail.com>
2024-02-27 18:24:08 +0100

    Add new line at the end of ths file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,5 +6,5 @@ process.stdin.on('readable', () => {
   }
 });
 process.stdin.on('close', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
