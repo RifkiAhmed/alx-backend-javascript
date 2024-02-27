36c88cac0f1f4010edc8b01d01960e11ba116503
ahmed <rifkiahmed.91@gmail.com>
2024-02-27 21:46:59 +0100

    Fix requirement check

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,10 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (_, response) => {
-    response.send('Hello Holberton School!');
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
 });
-
 app.listen(1245);
 
 module.exports = app;
