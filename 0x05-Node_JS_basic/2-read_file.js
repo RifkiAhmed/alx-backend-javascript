00143f31a0b7e455a96af4cf6b38a1b1837a6e0b
ahmed <rifkiahmed.91@gmail.com>
2024-02-27 18:39:27 +0100

    Read a file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const students = data.split('\n').filter((line) => line.trim().length > 0).slice(1);
+    const students = data.split('\n').filter((line) => line.length > 0).slice(1);
     console.log(`Number of students: ${students.length}`);
-    const fields = new Set(students.map((student) => student.split(',')[3].trim()));
+    const fields = new Set(students.map((student) => student.split(',')[3]));
     fields.forEach((field) => {
-      const fieldStudents = students.filter((student) => student.split(',')[3].trim() === field);
-      const firstNames = fieldStudents.map((student) => student.split(',')[0].trim()).join(', ');
+      const fieldStudents = students.filter((student) => student.split(',')[3] === field);
+      const firstNames = fieldStudents.map((student) => student.split(',')[0]).join(', ');
       const numberOfStudents = fieldStudents.length;
       console.log(`Number of students in ${field}: ${numberOfStudents}. List: ${firstNames}`);
     });
