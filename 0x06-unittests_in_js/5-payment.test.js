6d69210460bbd654b1c9a040046010c3395caaea
ahmed <rifkiahmed.91@gmail.com>
2024-02-29 00:59:04 +0100

    Use beforeEach and afterEach hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  const consoleSpy = sinon.spy(console, 'log');
+  beforeEach(() => {
+    consoleSpy.resetHistory();
+  });
+
+  afterEach(() => {
+    // consoleSpy.restore();
+  });
+
+  it('should call Utils.calculateNumber with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('should call Utils.calculateNumber with 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
