8e65fa8b4d0b51374343d37b792a90b82dfd4006
ahmed <rifkiahmed.91@gmail.com>
2024-02-28 20:30:19 +0100

    Fix ESLint error

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
+
+const express = require('express');
 
 const router = express.Router();
 
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,5 +1,6 @@
-const express = require('express');
 import routes from './routes';
+
+const express = require('express');
 
 const app = express();
 
