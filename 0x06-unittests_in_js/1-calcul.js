3877dc6ac3de95fe07a5e238405526a81731e0b0
ahmed <rifkiahmed.91@gmail.com>
2024-02-28 23:08:50 +0100

    Perfom an operation of two numbers rounded based on the value of type argument

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,21 @@
+function calculateNumber(type, a, b) {
+  switch (type) {
+    case 'SUM':
+      return Math.round(a) + Math.round(b);
+      break;
+    case 'SUBTRACT':
+      return Math.round(a) - Math.round(b);
+      break;
+    case 'DIVIDE':
+      if (Math.round(b) === 0) {
+        return 'Error';
+        break;
+      }
+      return Math.round(a) / Math.round(b);
+      break;
+    }
+    
+  }
+  
+  module.exports = calculateNumber;
+  
