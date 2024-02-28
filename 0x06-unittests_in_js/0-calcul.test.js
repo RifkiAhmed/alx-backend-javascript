cb9075187787c8e09e3b8d1807e0d7f435d87eb0
ahmed <rifkiahmed.91@gmail.com>
2024-02-28 23:11:52 +0100

    Unittests of 0-calcul.js code

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,62 +2,30 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 // Test cases
-it('calculateNumber(1, 3) should return 4', () => {
-  assert.equal(calculateNumber(1, 3), 4);
-});
+describe('calculateNumber', () => {
+  it('should return the sum of integer numbers', () => {
+    assert.equal(calculateNumber(0, 3), 3);
+    assert.equal(calculateNumber(1, 0), 1);
+    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(-1, -3), -4);
+  });
 
-it('calculateNumber(0.1, 3.7) should return 4', () => {
-  assert.equal(calculateNumber(0.1, 3.7), 4);
-});
+  it('should return the sum of rounded numbers', () => {
+    assert.equal(calculateNumber(1, 3.5), 5);
+    assert.equal(calculateNumber(0.1, 3), 3);
+    assert.equal(calculateNumber(0.7, -3), -2);
+    assert.equal(calculateNumber(0.7, -3.5), -2);
+  });
 
-it('calculateNumber(1.2, 3.7) should return 5', () => {
-  assert.equal(calculateNumber(1.2, 3.7), 5);
-});
+  it('should return 0 when both numbers are 0', () => {
+    assert.equal(calculateNumber(0, 0), 0);
+  });
 
-it('calculateNumber(1.5, 3.7) should return 6', () => {
-  assert.equal(calculateNumber(1.5, 3.7), 6);
-});
+  it('should handle large numbers correctly', () => {
+    assert.equal(calculateNumber(1000000000000000000000.4, 0), 1000000000000000000000);
+    assert.equal(calculateNumber(0, 1000000000000000000000.5), 1000000000000000000001);
+    assert.equal(calculateNumber(1.1e20, 0), 1.1e20);
+    assert.equal(calculateNumber(0, -1.5e20), -1.5e20);
 
-it('calculateNumber(-1, -3) should return -4', () => {
-  assert.equal(calculateNumber(-1, -3), -4);
-});
-
-it('calculateNumber(-1.5, 2.5) should return 2', () => {
-  assert.equal(calculateNumber(-1.5, 2.5), 2);
-});
-
-it('calculateNumber(1.5, -2.5) should return 0', () => {
-  assert.equal(calculateNumber(1.5, -2.5), 0);
-});
-
-it('calculateNumber(-1.5, -2.5) should return -3', () => {
-  assert.equal(calculateNumber(-1.5, -2.5), -3);
-});
-
-it('calculateNumber(0, 0) should return 0', () => {
-  assert.equal(calculateNumber(0, 0), 0);
-});
-
-it('calculateNumber(0, 2.5) should return 3', () => {
-  assert.equal(calculateNumber(0, 2.5), 3);
-});
-
-it('calculateNumber(1.1, 0) should return 1', () => {
-  assert.equal(calculateNumber(1.1, 0), 1);
-});
-
-it('calculateNumber(1000000000000000000000.4, 0) should return 1000000000000000000000', () => {
-  assert.equal(calculateNumber(1000000000000000000000.4, 0), 1000000000000000000000);
-});
-
-it('calculateNumber(1000000000000000000000.5, 0) should return 1000000000000000000002', () => {
-  assert.equal(calculateNumber(1000000000000000000000.5, 0), 1000000000000000000002);
-});
-
-it('calculateNumber(1.1e20, 0) should return 1.1e20', () => {
-  assert.equal(calculateNumber(1.1e20, 0), 1.1e20);
-});
-
-it('calculateNumber(-1.5e20, 0) should return -1.5e20', () => {
-  assert.equal(calculateNumber(-1.5e20, 0), -1.5e20);
+  });
 });
