5b80d1b185abc3193ae33222e6cd02e70308be00
ahmed <rifkiahmed.91@gmail.com>
2024-02-29 01:19:54 +0100

    Test 6-payment_token.js code

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return the correct response for success', (done) => {
+    getPaymentTokenFromAPI(true)
+    .then((response) => {
+      expect(response.data).to.deep.equal('Successful response from the API');
+      done();
+    });
+  });
+});
