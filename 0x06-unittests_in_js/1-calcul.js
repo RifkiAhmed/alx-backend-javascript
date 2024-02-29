d06b09cff73f4cef6479ab2e725029379364f398
ahmed <rifkiahmed.91@gmail.com>
2024-02-29 22:51:48 +0100

    Remove break statments;

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,20 +2,14 @@ function calculateNumber(type, a, b) {
   switch (type) {
     case 'SUM':
       return Math.round(a) + Math.round(b);
-      break;
     case 'SUBTRACT':
       return Math.round(a) - Math.round(b);
-      break;
     case 'DIVIDE':
       if (Math.round(b) === 0) {
         return 'Error';
-        break;
       }
       return Math.round(a) / Math.round(b);
-      break;
-    }
-    
   }
-  
-  module.exports = calculateNumber;
-  
+}
+
+module.exports = calculateNumber;
