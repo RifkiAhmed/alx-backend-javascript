faa2f0d3b559af49df44384dee0e87f94c8e98b2
ahmed <rifkiahmed.91@gmail.com>
2024-02-29 17:18:55 +0100

    Test cases for 0-calcul.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,9 +3,9 @@ const calculateNumber = require('./0-calcul');
 
 // Test cases
 it('calculateNumber(-0.1, -3) should return -3', () => {
-  assert.equal(calculateNumber(-1.7, -2), -4);
+  assert.equal(calculateNumber(-1.1, -2), -3);
 });
 
 it('calculateNumber(-1.5, 3.5) should return 3', () => {
-  assert.equal(calculateNumber(-1, -2.7), -4);
+  assert.equal(calculateNumber(0, -2.7), -3);
 });
