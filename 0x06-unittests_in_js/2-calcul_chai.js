71a91f6fbfa73844793ef79775b6fc79d86f6f48
ahmed <rifkiahmed.91@gmail.com>
2024-02-29 22:47:41 +0100

    My code

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,11 +1,17 @@
-const calculateNumber = (type, a , b) => {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  } else if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  } else if (type === 'DIVIDE') {
-      if (Math.round(b) === 0) return 'Error';
-    return Math.round(a) / Math.round(b);
-  }
-};
-module.exports = calculateNumber;
+function calculateNumber(type, a, b) {
+    switch (type) {
+      case 'SUM':
+        return Math.round(a) + Math.round(b);
+      case 'SUBTRACT':
+        return Math.round(a) - Math.round(b);
+      case 'DIVIDE':
+        if (Math.round(b) === 0) {
+          return 'Error';
+        }
+        return Math.round(a) / Math.round(b);
+      }
+      
+    }
+    
+    module.exports = calculateNumber;
+    
