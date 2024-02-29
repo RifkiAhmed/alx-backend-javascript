1ce77be635463fd944a0299597d75e99d6fd8ce3
ahmed <rifkiahmed.91@gmail.com>
2024-02-29 22:47:15 +0100

    My code

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,68 +1,70 @@
-const calculateNumber = require('./2-calcul_chai.js');
-const chai = require('chai');
-const expect = chai.expect;
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
-
-describe('test for calculateNumber - SUM', () => {
-  it('should get the sum of integers', () => {
-    expect(calculateNumber('SUM', 1, 1)).to.equal(2);
-    expect(calculateNumber('SUM', 1, 2)).to.equal(3);
-    expect(calculateNumber('SUM', 3, -2)).to.equal(1);
-    expect(calculateNumber('SUM', -9, 4)).to.equal(-5);
+// Test cases
+describe('calculateNumber', () => {
+  it('should return the sum of integer numbers', () => {
+    expect(calculateNumber('SUM', 0, 3)).to.equal(3);
+    expect(calculateNumber('SUM', 1, 0)).to.equal(1);
+    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+    expect(calculateNumber('SUM', -1, -3)).to.equal(-4);
   });
 
-  it('should round floats and calulate the sum', () => {
-    expect(calculateNumber('SUM', 1.2, 4.4)).to.equal(5);
-    expect(calculateNumber('SUM', 4, 8.8)).to.equal(13);
-    expect(calculateNumber('SUM', -3.5, 5)).to.equal(2);
-    expect(calculateNumber('SUM', 1, 3.8)).to.equal( 5);
-    expect(calculateNumber('SUM', 2.5, 4.6)).to.equal(8);
-    expect(calculateNumber('SUM', -5.9, -6)).to.equal(-12);
-  });
-});
-
-
-describe('test for calculateNumber - SUBTRACT', () => {
-  it('should get the difference of integers', () => {
-    expect(calculateNumber('SUBTRACT', 1, 1)).to.equal( 0);
-    expect(calculateNumber('SUBTRACT', 1, 2)).to.equal( -1);
-    expect(calculateNumber('SUBTRACT', 3, -2)).to.equal( 5);
-    expect(calculateNumber('SUBTRACT', -9, 4)).to.equal( -13);
+  it('should return the sum of rounded numbers', () => {
+    expect(calculateNumber('SUM', 1, 3.5)).to.equal(5);
+    expect(calculateNumber('SUM', 0.1, 3)).to.equal(3);
+    expect(calculateNumber('SUM', 0.7, -3)).to.equal(-2);
+    expect(calculateNumber('SUM', 0.7, -3.5)).to.equal(-2);
   });
 
-  it('should round floats and calulate the difference', () => {
-    expect(calculateNumber('SUBTRACT',1.2, 4.4)).to.equal( -3);
-    expect(calculateNumber('SUBTRACT',4, 8.8)).to.equal(-5);
-    expect(calculateNumber('SUBTRACT',-3.5, 5)).to.equal(-8);
-    expect(calculateNumber('SUBTRACT', 1, 3.8)).to.equal(-3);
-    expect(calculateNumber('SUBTRACT', 2.5, 4.6)).to.equal(-2);
-    expect(calculateNumber('SUBTRACT', -5.9, -6)).to.equal(0);
+  it('should return the subtract of integer numbers', () => {
+    expect(calculateNumber('SUBTRACT', 0, 3)).to.equal(-3);
+    expect(calculateNumber('SUBTRACT', 1, 0)).to.equal(1);
+    expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
+    expect(calculateNumber('SUBTRACT', -1, -3)).to.equal(2);
   });
 
-});
-
-
-
-describe('test for calculateNumber - DIVIDE', () => {
-  it('should get the division output of integers', () => {
-    expect(calculateNumber('DIVIDE', 1, 1)).to.equal(1);
-    expect(calculateNumber('DIVIDE', 1, 2)).to.equal(0.5);
-    expect(calculateNumber('DIVIDE', 3, -2)).to.equal(-1.5);
-    expect(calculateNumber('DIVIDE', -9, 3)).to.equal(-3);
+  it('should return the subtract of rounded numbers', () => {
+    expect(calculateNumber('SUBTRACT', 1, 3.5)).to.equal(-3);
+    expect(calculateNumber('SUBTRACT', 0.1, 3)).to.equal(-3);
+    expect(calculateNumber('SUBTRACT', 0.7, -3)).to.equal(4);
+    expect(calculateNumber('SUBTRACT', 0.7, -3.5)).to.equal(4);
   });
 
-  it('should round floats and calulate the quotient', () => {
-    expect(calculateNumber('DIVIDE', 1.2, 4.4)).to.equal(0.25);
-    expect(calculateNumber('DIVIDE', -3.5, 5)).to.equal(-0.6);
-    expect(calculateNumber('DIVIDE', 1, 3.8)).to.equal(0.25);
-    expect(calculateNumber('DIVIDE', 2.5, 5.6)).to.equal(0.5);
-    expect(calculateNumber('DIVIDE', -5.9, -6)).to.equal(1);
+  it('should return the divide of integer numbers', () => {
+    expect(calculateNumber('DIVIDE', 0, 3)).to.equal(0);
+    expect(calculateNumber('DIVIDE', 1, 3)).to.equal(0.3333333333333333);
+    expect(calculateNumber('DIVIDE', -1, -3)).to.equal(0.3333333333333333);
   });
 
-  it('should return error when denominator is zero', () => {
-    expect(calculateNumber('DIVIDE', 1.2, 0)).to.equal('Error');
-    expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
-    expect(calculateNumber('DIVIDE', -3.5, 0)).to.equal('Error');
+  it('should return the divide of rounded numbers', () => {
+    expect(calculateNumber('DIVIDE', 1, 3.5)).to.equal(0.25);
+    expect(calculateNumber('DIVIDE', 0.1, 3)).to.equal(0);
+    expect(calculateNumber('DIVIDE', 0.7, -3)).to.equal(-0.3333333333333333);
+    expect(calculateNumber('DIVIDE', 0.7, -3.5)).to.equal(-0.3333333333333333);
   });
 
+  it('should return Error when divisor is equal to 0', () => {
+    expect(calculateNumber('DIVIDE', 0, 0)).to.equal('Error');
+    expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error');
+    expect(calculateNumber('DIVIDE', -1, 0)).to.equal('Error');
+    expect(calculateNumber('DIVIDE', 0.7, 0)).to.equal('Error');
+    expect(calculateNumber('DIVIDE', 0.7, 0.4)).to.equal('Error');
+  });
+
+  it('should return 0 when both numbers are 0', () => {
+    expect(calculateNumber('SUM', 0, 0)).to.equal(0);
+    expect(calculateNumber('SUBTRACT', 0, 0)).to.equal(0);
+  });
+
+  it('should handle large numbers correctly', () => {
+    expect(calculateNumber('SUM', 1000000000000000000000.4, 0)).to.equal(1000000000000000000000);
+    expect(calculateNumber('SUM', 0, 1000000000000000000000.5)).to.equal(1000000000000000000001);
+    expect(calculateNumber('SUBTRACT', 1000000000000000000000.4, 0)).to.equal(1000000000000000000000);
+    expect(calculateNumber('SUBTRACT', 0 ,1000000000000000000000.5)).to.equal(-1000000000000000000001);
+    expect(calculateNumber('SUM', 1.1e20, 0)).to.equal(1.1e20);
+    expect(calculateNumber('SUM', 0, -1.5e20)).to.equal(-1.5e20);
+    expect(calculateNumber('SUBTRACT', 1.1e20, 0)).to.equal(1.1e20);
+    expect(calculateNumber('SUBTRACT', 0, -1.5e20)).to.equal(1.5e20);
+  });
 });
