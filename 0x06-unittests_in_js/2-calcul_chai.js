6b2975b09889628b33aca173c1e411d8c1a17831
ahmed <rifkiahmed.91@gmail.com>
2024-02-29 22:53:34 +0100

    Format document

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,17 +1,15 @@
 function calculateNumber(type, a, b) {
-    switch (type) {
-      case 'SUM':
-        return Math.round(a) + Math.round(b);
-      case 'SUBTRACT':
-        return Math.round(a) - Math.round(b);
-      case 'DIVIDE':
-        if (Math.round(b) === 0) {
-          return 'Error';
-        }
-        return Math.round(a) / Math.round(b);
+  switch (type) {
+    case 'SUM':
+      return Math.round(a) + Math.round(b);
+    case 'SUBTRACT':
+      return Math.round(a) - Math.round(b);
+    case 'DIVIDE':
+      if (Math.round(b) === 0) {
+        return 'Error';
       }
-      
-    }
-    
-    module.exports = calculateNumber;
-    
+      return Math.round(a) / Math.round(b);
+  }
+}
+
+module.exports = calculateNumber;
