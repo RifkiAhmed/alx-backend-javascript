af5058e4d32c3842dc288004c8d44997c8f3bb91
ahmed <rifkiahmed.91@gmail.com>
2024-02-29 17:57:24 +0100

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 describe('Index page', () => {
   it('request / route should return the index page content', (done) => {
-    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+    request.get('http://localhost:7865/', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
       done();
@@ -20,9 +20,9 @@ describe('Index page', () => {
 
   it('request /cart/hello route should return 404 not found error', (done) => {
     request.get('http://localhost:7865/cart/hello', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
+      expect(response.statusCode).to.equal(404);
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
-})
+});
